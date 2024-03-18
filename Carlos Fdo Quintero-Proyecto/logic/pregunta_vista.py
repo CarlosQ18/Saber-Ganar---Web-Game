d0619a3786b3b4c3d7fdae7c292898ef38fdada6
CarlosQ18 <38199997+CarlosQ18@users.noreply.github.com>
2024-03-17 21:04:42 -0500

    Delete Carlos Fdo Quintero-Proyecto directory

--- Carlos Fdo Quintero-Proyecto/logic/pregunta_vista.py
+++
@@ -1,8 +0,0 @@
-import os
-filepath=os.path.abspath(os.getcwd()) + '/logic/preguntastomadas.txt'
-def pregunta_vista(id_pregunta,correo,grado):
-  file=open(filepath,"a")
-  tabla=str(id_pregunta)+","+str(correo)+","+str(grado)
-  file.write(tabla)
-  file.write("\n")
-  
